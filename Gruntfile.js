c50a0ec02ab0e5ad9c419c4ed2b29f46790fb421
Kyle Conkright <kyleconkright@gmail.com>
2014-02-07 10:43:23 -0800

    Added / Configured Grunt Task Runner
    Grunt is pretty sweet. It will automate a bunch of stuff. Sort of like
    CodeKit but more customizable.

---
+++ Gruntfile.js
@@ -0,0 +1,52 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  	grunt.initConfig({
+	    pkg: grunt.file.readJSON('package.json'),
+	    
+	    sass: {
+	    	dist: {
+		      	options: {
+		        	style: 'compressed'
+		      	},
+		      	files: {
+		        	'tmp/global.css': 'lib/global.scss'
+		      	}
+		    }
+		},
+		autoprefixer: {
+            dist: {
+                files: {
+                    'build/css/global.css': 'tmp/global.css'
+                }
+            }
+        },
+        coffee: {
+          compile: {
+	            files: {
+	              	'tmp/script.js': 'lib/script.coffee'
+	            }
+          	}
+      	},
+      	uglify: {
+      		my_target: {
+      		    files: {
+      		    	'build/js/vendor.min.js': 'lib/vendor/*.js',
+      		    	'build/js/script.min.js': 'tmp/script.js'
+      		    }
+      		}
+	    }
+
+  	});
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify','sass','autoprefixer','coffee']);
+
+
+};
